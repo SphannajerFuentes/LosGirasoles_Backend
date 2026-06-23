9e5eedc3969c81a4ea32fe0a8f4e55f2c1eaeadb
SphannajerFuentes <sphannajerfuentes@gmail.com>
2026-06-23 00:31:04 -0500

    main

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-# logistica-backend/app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware 
-from app.controllers.AuthController import router as auth_router # Corregido el import
+from app.core.excepciones import configurar_manejadores_errores
+
+# Importación de enrutadores
+from app.controllers.AuthController import router as auth_router 
 from app.controllers.AdminController import router as admin_router
 from app.controllers.MedicamentoController import router as medic_router
 from app.controllers.OrdenCompraController import router as order_router
 from app.controllers.IncidenciaController import router as incidencia_router
 from app.controllers.OperacionesController import router as router_lotes
 
-app = FastAPI(title="API Logística Los Girasoles", version="1.0.0")
+app = FastAPI(title="API Logística Los Girasoles", version="2.0.0")
+
+# Activamos el manejador global de errores que creamos usando Herencia
+configurar_manejadores_errores(app)
 
 app.add_middleware(
     CORSMiddleware,
