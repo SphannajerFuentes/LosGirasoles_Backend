285e9e5074f9e33a29ef75331dc8738b5d520755
SphannajerFuentes <sphannajerfuentes@gmail.com>
2026-06-23 00:15:28 -0500

    OrdenCompraModel

--- app/models/OrdenCompraModel.py
+++ app/models/OrdenCompraModel.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 from datetime import date
 from typing import List
+from app.models.BaseModel import EntidadBase
 
 class DetalleOrden(BaseModel):
     id_medicamento: int
     cantidad: int
     precio_unitario: float
 
-class OrdenCompraCreate(BaseModel):
+class OrdenCompraCreate(EntidadBase):
     id_proveedor: int
     fecha_emision: date
     detalles: List[DetalleOrden]
