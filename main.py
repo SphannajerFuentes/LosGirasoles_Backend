53a362eff0fca5f457e93db38fa7d0975c3c5fa6
SphannajerFuentes <sphannajerfuentes@gmail.com>
2026-07-15 06:49:25 -0500

    Fix CORS

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ configurar_manejadores_errores(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","*"],
+    allow_origins=["http://localhost:5173","https://farmacialosgardines-pi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
